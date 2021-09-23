4f98e0aaa69c6ed41cd74f1dc2fed124b24dafb8
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2021-09-23 16:44:43 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# hypertrack-sdk-react-native-views
+# React Native HyperTrack SDK
+
+![GitHub](https://img.shields.io/github/license/hypertrack/sdk-react-native.svg)
+![npm](https://img.shields.io/npm/v/hypertrack-sdk-react-native.svg)
+[![iOS SDK](https://img.shields.io/badge/iOS%20SDK-4.7.0-brightgreen.svg)](https://cocoapods.org/pods/HyperTrack)
+![Android SDK](https://img.shields.io/badge/Android%20SDK-4.10.0-brightgreen.svg)
+
+[HyperTrack](https://www.hypertrack.com) lets you add live location tracking to your mobile app. Live location is made available along with ongoing activity, tracking controls and tracking outage with reasons.
+
+React Native HyperTrack SDK is a wrapper around native iOS and Android SDKs that allows to integrate them into React Native apps.
+
+For information about how to get started with React Native SDK, please visit this [HyperTrack Guide](https://www.hypertrack.com/docs/install-sdk-react-native).
---
+++ android/src/main/java/com/hypertrack/reactnative/androidsdk/HTSDKModule.java
@@ -0,0 +1,225 @@
+package com.hypertrack.reactnative.androidsdk;
+import android.util.Log;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.hypertrack.sdk.HyperTrack;
+import com.hypertrack.sdk.TrackingError;
+import com.hypertrack.sdk.TrackingStateObserver;
+import java.util.Locale;
+import com.hypertrack.sdk.views.HyperTrackViews;
+import androidx.core.util.Consumer;
+import com.google.gson.Gson;
+import com.hypertrack.sdk.views.DeviceUpdatesHandler;
+import com.hypertrack.sdk.views.dao.Location;
+import com.hypertrack.sdk.views.dao.MovementStatus;
+import com.hypertrack.sdk.views.dao.StatusUpdate;
+import com.hypertrack.sdk.views.dao.Trip;
+import androidx.annotation.NonNull;
+
+@ReactModule(name = HTSDKModule.NAME)
+public class HTSDKModule extends ReactContextBaseJavaModule {
+    private static final String TAG = "HTSDKModule";
+    public static final String NAME = "HyperTrack";
+    public TrackingStateObserver.OnTrackingStateChangeListener trackingStateChangeListener;
+    public HyperTrack sdkInstance;
+    public DeviceUpdatesHandler deviceUpdatesHandler;
+    public HyperTrackViews mHyperTrackView;
+     final Gson gson = new Gson();
+    public HTSDKModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    @Override
+    public String getName() {
+        return NAME;
+    }
+    @ReactMethod
+    public void initialize(String publishableKey, Boolean startsTracking, Boolean automaticallyRequestPermissions, Promise promise) {
+        try {
+            sdkInstance = HyperTrack.getInstance(
+                    publishableKey
+            );
+            if (startsTracking) {
+                sdkInstance.start();
+            }
+            mHyperTrackView= HyperTrackViews.getInstance(getReactApplicationContext().getApplicationContext() , publishableKey);
+            promise.resolve(true);
+        } catch (java.lang.Exception exception) {
+            Log.e(TAG, "Hypertrack SDK initialization failed.",  exception);
+            String error = String.format(Locale.ENGLISH, "%d", RN_ERROR_INVALID_PUBLISHABLE_KEY);
+            promise.reject(error, new Exception("Hypertrack SDK initialization failed."));
+        }
+    }
+    @ReactMethod
+    public void subscribeOnEvents() {
+        if (trackingStateChangeListener != null) {
+            sdkInstance.removeTrackingListener(trackingStateChangeListener);
+            trackingStateChangeListener = null;
+        }
+        trackingStateChangeListener = new TrackingStateObserver.OnTrackingStateChangeListener() {
+            @Override
+            public void onError(TrackingError trackingError) {
+                WritableMap params = Arguments.createMap();
+                params.putInt("code", convertToRNErrorsCode(trackingError.code));
+                params.putString("message", trackingError.message);
+                getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onTrackingErrorHyperTrack", params);
+            }
+            @Override
+            public void onTrackingStart() {
+                getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onTrackingStartHyperTrack", null);
+            }
+            @Override
+            public void onTrackingStop() {
+                getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onTrackingStopHyperTrack", null);
+            }
+        };
+        sdkInstance.addTrackingListener(trackingStateChangeListener);
+    }
+    @ReactMethod
+    public void getDeviceID(Promise promise) {
+        promise.resolve(sdkInstance.getDeviceID());
+    }
+    @ReactMethod
+    public void isTracking(Promise promise) {
+        promise.resolve(sdkInstance.isRunning());
+    }
+    @ReactMethod
+    public void startTracking() {
+        sdkInstance.start();
+    }
+    @ReactMethod
+    public void stopTracking() {
+        sdkInstance.stop();
+    }
+    @ReactMethod
+    public void syncDeviceSettings() {
+        sdkInstance.syncDeviceSettings();
+    }
+    @ReactMethod
+    public void setTripMarker(ReadableMap rMap, Promise promise) {
+        try {
+            sdkInstance.addGeotag(rMap.toHashMap());
+            promise.resolve(null);
+        } catch (Exception e) {
+            String error = String.format(Locale.ENGLISH, "%d", RN_ERROR_GENERAL);
+            promise.reject(error, e);
+        }
+    }
+    @ReactMethod
+    public void setDeviceName(String name, Promise promise) {
+        try {
+            sdkInstance.setDeviceName(name);
+            promise.resolve(true);
+        } catch (Exception e) {
+            String error = String.format(Locale.ENGLISH, "%d", RN_ERROR_GENERAL);
+            promise.reject(error, e);
+        }
+    }
+    @ReactMethod
+    public void setMetadata(ReadableMap rMap, Promise promise) {
+        try {
+            sdkInstance.setDeviceMetadata(rMap.toHashMap());
+            promise.resolve(true);
+        } catch (Exception e) {
+            String error = String.format(Locale.ENGLISH, "%d", RN_ERROR_GENERAL);
+            promise.reject(error, e);
+        }
+    }
+    @ReactMethod
+    public void enableDebugLogging(Boolean enable) {
+        if (enable) {
+            HyperTrack.enableDebugLogging();
+        }
+    }
+    @ReactMethod
+    public void enableMockLocation(Boolean enable) {
+        if (enable) {
+            sdkInstance.allowMockLocations();
+        }
+    }
+   @ReactMethod
+    public void getDeviceMovementStatus(String deviceID, final Promise promise){
+    mHyperTrackView.getDeviceMovementStatus(deviceID,
+                new Consumer<MovementStatus>() {
+                    @Override
+                    public void accept(MovementStatus movementStatus) { 
+                        promise.resolve(gson.toJson(movementStatus));
+                    }
+                });
+    }
+    @ReactMethod
+    public void subscribeToDeviceUpdates(String deviceID, final Promise promise){
+        if (deviceUpdatesHandler != null) {
+            mHyperTrackView.stopAllUpdates();
+            deviceUpdatesHandler = null;
+        }
+    mHyperTrackView.subscribeToDeviceUpdates(deviceID,
+                deviceUpdatesHandler=new DeviceUpdatesHandler() {
+                    @Override
+                    public void onLocationUpdateReceived(@NonNull Location location) {
+                       getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onLocationUpdateReceived", gson.toJson(location));
+                    }
+                    @Override
+                    public void onBatteryStateUpdateReceived(@MovementStatus.BatteryState int i) {
+                       getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onBatteryStateUpdateReceived", i);
+                    }
+                    @Override
+                    public void onStatusUpdateReceived(@NonNull StatusUpdate statusUpdate) {
+                         getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onStatusUpdateReceived", gson.toJson(statusUpdate));
+                    }
+                    @Override
+                    public void onTripUpdateReceived(@NonNull Trip trip) {
+                       getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onTripUpdateReceived", gson.toJson(trip));
+                    }
+                    @Override
+                    public void onError(Exception exception, String deviceId) {
+                        getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onErrorDeviceUpdates", exception);
+                    }
+                    @Override
+                    public void onCompleted(String deviceId) {
+                         getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onCompletedDeviceUpdates", deviceId);
+                    }
+                });
+}
+   
+    private static final int RN_ERROR_GENERAL = 0;
+    private static final int RN_ERROR_INVALID_PUBLISHABLE_KEY = 1;
+    private static final int RN_ERROR_AUTHORIZATION_FAILED = 2;
+    private static final int RN_ERROR_PERMISSION_DENIED = 3;
+    private int convertToRNErrorsCode(int androidSdkCode) {
+        switch (androidSdkCode) {
+            case TrackingError.INVALID_PUBLISHABLE_KEY_ERROR:
+                return RN_ERROR_INVALID_PUBLISHABLE_KEY;
+            case TrackingError.AUTHORIZATION_ERROR:
+                return RN_ERROR_AUTHORIZATION_FAILED;
+            case TrackingError.PERMISSION_DENIED_ERROR:
+                return RN_ERROR_PERMISSION_DENIED;
+            default:
+                return RN_ERROR_GENERAL;
+        }
+    }
+}
